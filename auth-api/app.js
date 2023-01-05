3e44eeab67aac853376fe605562e87c47a17aba4
renatostefani <renatostef@gmail.com>
2023-01-05 18:12:40 -0300

    Commit inicial
    Commit inicial

---
+++ auth-api/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const env = process.env;
+const PORT = env.PORT || 8080;
+
+app.get('/api/status', (req, res) => {
+    return res.status(200).json({
+        service: "Auth-API",
+        status: "up"
+    });
+});
+
+app.listen(PORT, () => {
+    console.info(`Servidor inicializado com sucesso na porta ${PORT}`);
+});
